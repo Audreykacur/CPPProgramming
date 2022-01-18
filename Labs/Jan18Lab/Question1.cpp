007d8f1a3358055f862bbdde4a110ba3c29b7919
Audreykacur <audreykacur@gmail.com>
2022-01-18 16:14:50 -0600

    adding folders

---
+++ Labs/Jan18Lab/Question1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    a = 3;
+    b = 4;
+
+    c = a + b;
+    cout << "The sum of a and b is: " << c << endl;
+
+    return 0;
+}
